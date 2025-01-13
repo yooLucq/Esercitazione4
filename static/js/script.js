04237222930b8b8d224713d34a521b3a639c8492
yooLucq <lucacoppolecchia07@gmail.com>
2025-01-13 17:29:36 +0100

    .

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,14 @@
-let elements
-document.querySelectorAll(elements)
+let elements = document.querySelectorAll(".el");
+
+
+elements.forEach(element => {
+    element.addEventListener('click', function(event) {
+        
+        const clickedElement = event.currentTarget;
+
+        if (clickedElement.classList.contains("green")){
+            clickedElement.classList.remove("green");
+            clickedElement.classList.add("yellow");
+        }
+    });
+});
